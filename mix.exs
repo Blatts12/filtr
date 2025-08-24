08aa03a87a519c60de41522cdb5ecf51ae83e73d
Jakub Melkowski <9402720+Blatts12@users.noreply.github.com>
2025-08-24 18:10:26 +0200

    Change elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pex.MixProject do
     [
       app: :pex,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
